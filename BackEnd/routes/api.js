1f125765a6df03034a34f031c730939fc3176cda
YevgenyBorko <79790637+YevgenyBorko@users.noreply.github.com>
2021-08-11 17:57:40 +0300

    Update api.js

--- BackEnd/routes/api.js
+++ BackEnd/routes/api.js
@@ -1,4 +1,4 @@
-//************************************************************Global section***********************************************************/
+//************************************************************Global section**********************************************************/
 require('../db/db');
 var config = require('../configService');
 var assistant = require('../webAssistant');
@@ -13,32 +13,39 @@ const { response } = require('express');
 const { isValidObjectId } = require('mongoose');
 router.use(bodyParser.json()); // support json encoded bodies
 router.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
- 
-//**********************************************************Query router**********************************************************/
+//***********************************************************************************************************************************/
+
+//***********************************************************Query router************************************************************/
 router.post('/query', function(req, res, next) {
     var myAssistant = new assistant();
     var DB = new dbSearcher();
+    //Here we use the assistent service to create queries array.
     let queries = myAssistant.createQueriesArray(new Date(req.body.start), new Date(req.body.end), req.body.make, req.body.model, 
                                                     req.body.color, req.body.carType, req.body.shirtColor, req.body.pantsColor, 
                                                     req.body.searchHuman, req.body.searchCar);                                                                                                                                                                                                     
-    DB.query(queries[0], queries[1], req.body.cam, req.body.searchHuman, req.body.searchCar, req.body.cam);
-    res.send(JSON.stringify((queries[0].length + queries[1].length) * req.body.cam.length));
+    DB.query(queries[0], queries[1], req.body.cam, req.body.searchHuman, req.body.searchCar, req.body.cam);//call DBsearch service to search in the DB.
+    res.send(JSON.stringify((queries[0].length + queries[1].length) * req.body.cam.length));//send to the user the number of the results that he should wait
 });
+//***********************************************************************************************************************************/
 
-//*******************************************************Send cameras list router******************************************************/
-router.get('/getCameras', function(req, res, next) {    //
-    var x = new config()                                //
-    camList = x.getCamsMap();                           // Here the server get a list of 
-    var newList = []                                    // camera names and send it to UI.
-    for(i in camList){                                  //
-          newList.push(camList[i][0])                               // *the UI call this rout during 
-    }                                                   //  it's upload for build it's 
-     res.send(newList)                                  //  camers list.
+//*******************************************************Send cameras list router****************************************************/
+//The client call this route during it's upload for build it's camers list.
+router.get('/getCameras', function(req, res, next) {    
+    var x = new config()                                
+    camList = x.getCamsMap();// Here the server get a map of cameras information. Then it create a list of cameras names. 
+    var newList = []                                    
+    for(i in camList){                                  
+          newList.push(camList[i][0])                               
+    }                                                   
+     res.send(newList) //send the list of cameras names to the client.                               
 });
+//***********************************************************************************************************************************/
 
+//**********************************************************KeepAlive router**********************************************************/
 router.get('/status', function(req, res, next) {     
     var answer = 'OK'                                                                                
      res.send(answer);                                  
 });  
+//***********************************************************************************************************************************/
 
 module.exports = router;
